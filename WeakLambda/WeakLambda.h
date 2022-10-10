9409637ae7132e176fd410a20f994637f535bf3f
lenomirei <lenomirei@163.com>
2022-10-10 18:04:07 +0800

    [modify] Modify template name

--- WeakLambda/WeakLambda.h
+++ WeakLambda/WeakLambda.h
@@ -1,18 +1,16 @@
 #pragma once
 
-#include <functional>
-
-template <typename T, typename F>
+template <typename T, typename Func>
 class WeakLambda
 {
 public:
     WeakLambda() = delete;
 
-    WeakLambda(T* raw_obj, const F& func) : func_(func), weak_obj_(raw_obj->shared_from_this())
+    WeakLambda(T* raw_obj, const Func& func) : func_(func), weak_obj_(raw_obj->shared_from_this())
     {
     }
 
-    WeakLambda(const std::shared_ptr<T>& ptr, const F& func) : func_(func), weak_obj_(ptr)
+    WeakLambda(const std::shared_ptr<T>& ptr, const Func& func) : func_(func), weak_obj_(ptr)
     {
     }
 
@@ -39,7 +37,7 @@ public:
     }
 
     template<typename... ArgTypes>
-    auto operator()(ArgTypes... args) -> typename std::result_of<F(ArgTypes...)>::type
+    auto operator()(ArgTypes... args) -> typename std::result_of<Func(ArgTypes...)>::type
     {
         std::shared_ptr<T> obj = weak_obj_.lock();
         if (obj)
@@ -52,7 +50,7 @@ public:
         }
     }
 private:
-    F func_;
+    Func func_;
     std::weak_ptr<T> weak_obj_;
 };
 
@@ -63,15 +61,15 @@ public:
     class Holder : public std::enable_shared_from_this<Holder>
     {};
 
-    template<typename Functor>
-    WeakLambda<Holder, Functor> CreateWeakLambda(const Functor& lambda)
+    template<typename Func>
+    WeakLambda<Holder, Func> CreateWeakLambda(const Func& lambda)
     {
         if (!holder_)
         {
             holder_ = std::make_shared<Holder>();
         }
 
-        return WeakLambda<Holder, Functor>(holder_, lambda);
+        return WeakLambda<Holder, Func>(holder_, lambda);
     }
 
 private:
@@ -81,25 +79,25 @@ private:
 template<typename T, bool>
 struct WeakLambdaHelper
 {   
-    template<typename Functor>
-    static WeakLambda<T, Functor> CreateWeakLambda(T* pointer, Functor lambda)
+    template<typename Func>
+    static WeakLambda<T, Func> CreateWeakLambda(T* pointer, Func lambda)
     {
-        return WeakLambda<T, Functor>(pointer, lambda);
+        return WeakLambda<T, Func>(pointer, lambda);
     }
 };
 
 template<typename T>
 struct WeakLambdaHelper<T, true>
 {
-    template<typename Functor>
-    static WeakLambda<EnableWeakLambdaCapture::Holder, Functor> CreateWeakLambda(T* pointer, Functor lambda)
+    template<typename Func>
+    static WeakLambda<EnableWeakLambdaCapture::Holder, Func> CreateWeakLambda(T* pointer, Func lambda)
     {
         return pointer->CreateWeakLambda(lambda);
     }
 };
 
-template<typename T, typename Functor>
-auto weak_lambda(T* pointer, Functor lambda) -> WeakLambda<typename std::conditional<std::is_base_of<EnableWeakLambdaCapture, T>::value, EnableWeakLambdaCapture::Holder, T>::type, Functor>
+template<typename T, typename Func>
+auto weak_lambda(T* pointer, Func lambda) -> WeakLambda<typename std::conditional<std::is_base_of<EnableWeakLambdaCapture, T>::value, EnableWeakLambdaCapture::Holder, T>::type, Func>
 {
     return WeakLambdaHelper<T, std::is_base_of<EnableWeakLambdaCapture, T>::value>::CreateWeakLambda(pointer, lambda);
 }
